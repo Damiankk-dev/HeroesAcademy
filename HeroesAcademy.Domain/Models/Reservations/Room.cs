274186d4fbddaf36592c7e10e2a58c41d1056ee1
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2023-03-19 19:35:04 +0100

    Accesory and room classes added

---
+++ HeroesAcademy.Domain/Models/Reservations/Room.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroesAcademy.Domain.Models.Reservations
+{
+    public class Room
+    {
+       public int Id { get; set; }
+        public string Name { get; set; }
+        public int Volume { get; set; }
+        public ICollection<Accessory> Accessories { get; set; }
+        public ICollection<Reservation> Reservations{ get; set; }
+    }
+}
