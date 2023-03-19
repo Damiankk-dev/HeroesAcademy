7f62ec5a6e6f121a88a3092d89a7289be9bd3776
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2023-03-19 19:16:42 +0100

    Fileld a tenant class

--- HeroesAcademy.Domain/Models/Reservations/Tenant.cs
+++ HeroesAcademy.Domain/Models/Reservations/Tenant.cs
@@ -1,6 +1,13 @@
-﻿namespace HeroesAcademy.Domain.Models.Reservations
+﻿using HeroesAcademy.Domain.Models.Heroes;
+
+namespace HeroesAcademy.Domain.Models.Reservations
 {
     public class Tenant
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Reservation>? Reservations { get; set; }
+        public Hero Hero { get; set; }
+
     }
 }
