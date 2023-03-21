9b1b69375a076e32cce2e4eda023eb16a4cfe458
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2023-03-21 19:17:49 +0100

    Added an interface of reservation repository

---
+++ HeroesAcademy.Application/Repository/IReservationRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using HeroesAcademy.Domain.Models.Heroes;
+using HeroesAcademy.Domain.Models.Reservations;
+
+namespace HeroesAcademy.Application.Repository
+{
+    public interface IReservationRepository
+    {
+        Task<ResponseResult<List<Reservation>>> GetByRoomId(int id);
+        Task<ResponseResult<List<Reservation>>> GetByUserId(int id);
+        Task<ResponseResult<Reservation>> Add(Reservation reservation);
+        Task<bool> Delete(int id);
+        Task<ResponseResult<Reservation>> Update(int id, Reservation reservation);
+    }
+}
