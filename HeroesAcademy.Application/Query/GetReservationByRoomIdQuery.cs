1c2d84ab8bba13dcbe51601523318776a2b2ed50
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2023-03-23 19:40:55 +0100

    Addde reservation controller adn get resrevation by room id query classes

---
+++ HeroesAcademy.Application/Query/GetReservationByRoomIdQuery.cs
@@ -0,0 +1,32 @@
+﻿using HeroesAcademy.Application.Repository;
+using HeroesAcademy.Domain.Models.Heroes;
+using HeroesAcademy.Domain.Models.Reservations;
+using MediatR;
+
+namespace HeroesAcademy.Application.Query
+{
+    public class GetReservationByRoomIdQuery:IRequest<ResponseResult<List<Reservation>>>
+    {
+        public int Id { get; }
+
+        public GetReservationByRoomIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+
+    public class GetByRoomIdQueryHandler : IRequestHandler<GetReservationByRoomIdQuery, ResponseResult<List<Reservation>>>
+    {
+        private readonly IReservationRepository _reservationRepository;
+
+        public GetByRoomIdQueryHandler(IReservationRepository reservationRepository)
+        {
+            _reservationRepository = reservationRepository;
+        }
+
+        public Task<ResponseResult<List<Reservation>>> Handle (GetReservationByRoomIdQuery request, CancellationToken cancellationToken)
+        {
+            return _reservationRepository.GetReservationByRoomId(request.Id);
+        }
+    }
+}
---
+++ HeroesAcademy/Controllers/ReservationsController.cs
@@ -0,0 +1,33 @@
+﻿using HeroesAcademy.Application.Query;
+using HeroesAcademy.Domain.Models.Reservations;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeroesAcademy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : BaseController
+    {
+        private readonly IMediator _mediator;
+
+        public ReservationsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("ByRoom/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Reservation))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(Reservation))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(int))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(int))]
+        public async Task<IActionResult> GetReservationByRoomId(int id)
+        {
+            var response = await _mediator.Send(new GetReservationByRoomIdQuery(id));
+            return OkOrError(response);
+        }
+    }
+}
