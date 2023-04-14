b3717758b9eab0447a8d757d01fe3f787ddc0bdf
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2023-04-14 09:05:12 +0200

    Fixed reservation api

--- ReservationsAPI/Controllers/ReservationsController.cs
+++ ReservationsAPI/Controllers/ReservationsController.cs
@@ -7,96 +7,95 @@ using Microsoft.AspNetCore.Mvc;
 using Reservations.Application.Commands;
 using Reservations.Application.Query;
 
-namespace HeroesAcademy.Controllers
+namespace ReservationsAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ReservationsController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ReservationsController : BaseController
+    private readonly IMediator _mediator;
+
+    public ReservationsController(IMediator mediator)
     {
-        private readonly IMediator _mediator;
+        _mediator = mediator;
+    }
 
-        public ReservationsController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
+    [AllowAnonymous]
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Reservation))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(Reservation))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(int))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(int))]
+    public async Task<IActionResult> GetReservation(int id)
+    {
+        var response = await _mediator.Send(new GetReservationByIdQuery(id));
+        return OkOrError(response);
+    }
+    [AllowAnonymous]
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Reservation))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(Reservation))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(int))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(int))]
+    public async Task<IActionResult> GetReservations()
+    {
+        var response = await _mediator.Send(new GetReservationsQuery());
+        return OkOrError(response);
+    }
 
-        [AllowAnonymous]
-        [HttpGet("{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Reservation))]
-        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(Reservation))]
-        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(int))]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(int))]
-        public async Task<IActionResult> GetReservation(int id)
-        {
-            var response = await _mediator.Send(new GetReservationByIdQuery(id));
-            return OkOrError(response);
-        }
-        [AllowAnonymous]
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Reservation))]
-        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(Reservation))]
-        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(int))]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(int))]
-        public async Task<IActionResult> GetReservations()
-        {
-            var response = await _mediator.Send(new GetReservationsQuery());
-            return OkOrError(response);
-        }
+    [AllowAnonymous]
+    [HttpGet("ByRoom/{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Reservation))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(Reservation))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(int))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(int))]
+    public async Task<IActionResult> GetReservationByRoomId(int id)
+    {
+        var response = await _mediator.Send(new GetReservationByRoomIdQuery(id));
+        return OkOrError(response);
+    }
 
-        [AllowAnonymous]
-        [HttpGet("ByRoom/{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Reservation))]
-        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(Reservation))]
-        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(int))]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(int))]
-        public async Task<IActionResult> GetReservationByRoomId(int id)
-        {
-            var response = await _mediator.Send(new GetReservationByRoomIdQuery(id));
-            return OkOrError(response);
-        }
+    [AllowAnonymous]
+    [HttpGet("ByHero/{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Reservation))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(Reservation))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(int))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(int))]
+    public async Task<IActionResult> GetResrevationByHeroId(int id)
+    {
+        var response = await _mediator.Send(new GetReservationByHeroIdQuery(id));
+        return OkOrError(response);
+    }
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(Reservation))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(Reservation))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> AddReservation([FromBody] Reservation reservation)
+    {
+        var response = await _mediator.Send(new AddReservationCommand(reservation));
+        return OkOrError(response);
+    }
 
-        [AllowAnonymous]
-        [HttpGet("ByHero/{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Reservation))]
-        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(Reservation))]
-        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(int))]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(int))]
-        public async Task<IActionResult> GetResrevationByHeroId(int id)
-        {
-            var response = await _mediator.Send(new GetReservationByHeroIdQuery(id));
-            return OkOrError(response);
-        }
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(Reservation))]
-        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(Reservation))]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> AddReservation([FromBody] Reservation reservation)
-        {
-            var response = await _mediator.Send(new AddReservationCommand(reservation));
-            return OkOrError(response);
-        }
-
-        [AllowAnonymous]
-        [HttpDelete("{reservationId}")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
-        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(int))]
-        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(int))]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(int))]
-        public async Task<IActionResult> DeleteReservation(int reservationId)
-        {
-            var response = await _mediator.Send(new DeleteReservationCommand(reservationId));
-            return OkOrError(response);
-        }
-        [AllowAnonymous]
-        [HttpPut("{reservationId}")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
-        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(int))]
-        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(int))]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(int))]
-        public async Task<IActionResult> UpdateReservation([FromBody] Reservation reservation, int reservationId)
-        {
-            var response = await _mediator.Send(new UpdateReservationCommand(reservationId, reservation));
-            return OkOrError(response);
-        }
+    [AllowAnonymous]
+    [HttpDelete("{reservationId}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(int))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(int))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(int))]
+    public async Task<IActionResult> DeleteReservation(int reservationId)
+    {
+        var response = await _mediator.Send(new DeleteReservationCommand(reservationId));
+        return OkOrError(response);
+    }
+    [AllowAnonymous]
+    [HttpPut("{reservationId}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(int))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(int))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(int))]
+    public async Task<IActionResult> UpdateReservation([FromBody] Reservation reservation, int reservationId)
+    {
+        var response = await _mediator.Send(new UpdateReservationCommand(reservationId, reservation));
+        return OkOrError(response);
     }
 }
