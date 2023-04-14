5e42b0a49d6c60c1f8000ed1aa47ca99d1a8f5ba
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2023-04-14 17:08:25 +0200

    Ocelot Configuration for a swagger and other http commands, with http(s) fix

--- OcelotApiGw/Program.cs
+++ OcelotApiGw/Program.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -11,33 +12,73 @@ namespace OcelotBasic
     {
         public static void Main(string[] args)
         {
-            new WebHostBuilder()
-            .UseKestrel()
-            .UseContentRoot(Directory.GetCurrentDirectory())
-            .ConfigureAppConfiguration((hostingContext, config) =>
-            {
-                config
-                    .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
-                    .AddJsonFile("appsettings.json", true, true)
-                    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
-                    .AddJsonFile("configuration.json")
-                    .AddEnvironmentVariables();
-            })
-            .ConfigureServices(s => {
-                s.AddOcelot();
-            })
-            .ConfigureLogging((hostingContext, logging) =>
-            {
-                logging.AddConsole();
-            })
-            .UseIISIntegration()
-            .Configure(app =>
-            {
-                app.UseOcelot().Wait();
-            })
-            .Build()
+            var builder = WebApplication.CreateBuilder(args);
 
-            .Run();
+            // Add services to the container.
+
+            builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("configuration.json")
+                .AddJsonFile("appsettings.json");
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddOcelot();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
+            var app = builder.Build();
+
+            //app.UseOcelot().Wait();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerForOcelotUI(opt => {
+                    opt.PathToSwaggerGenerator = "/swagger/docs";
+                })
+                    .UseOcelot()
+                    .Wait();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+            //builder
+            //builder.UseContentRoot(Directory.GetCurrentDirectory())
+            //.ConfigureAppConfiguration((hostingContext, config) =>
+            //{
+            //    config
+            //        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+            //        .AddJsonFile("appsettings.json", true, true)
+            //        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+            //        .AddJsonFile("configuration.json")
+            //        .AddEnvironmentVariables();
+            //})
+            //.ConfigureServices(s => {
+            //    s.AddOcelot();
+            //    s.AddSwaggerGen();
+            //    s.AddSwaggerForOcelot();
+            //})
+            //.ConfigureLogging((hostingContext, logging) =>
+            //{
+            //    logging.AddConsole();
+            //})
+            //.UseIISIntegration()
+            //.Configure(app =>
+            //{
+            //    app.UseOcelot().Wait();
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI(c => c.SwaggerEndpoint("v1/swagger.json", "Heroes Academy API"));
+            //})
+            //.Build()
+
+            //.Run();
         }
     }
 }
