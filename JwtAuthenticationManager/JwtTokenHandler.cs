b575ab819342bf52c25f12071c0a286e6d8e37bd
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2023-04-27 16:28:26 +0200

    Initial tests after added AuthApi

---
+++ JwtAuthenticationManager/JwtTokenHandler.cs
@@ -0,0 +1,64 @@
+﻿using HeroesAcademy.Application;
+using HeroesAcademy.Application.Repository.Heroes;
+using JwtAuthenticationManager.Models;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtAuthenticationManager
+{
+    public class JwtTokenHandler
+    {
+        public const string JWT_SECURITY_KEY = "HiperStrong$3curityKey";
+        private const int JWT_TOKEN_VALIDITY_MINS = 20;
+        private readonly List<UserAccount> _applicationUsers;
+
+        public JwtTokenHandler()
+        {
+            _applicationUsers = new List<UserAccount> { 
+                new UserAccount{ Username = "admin", Password="admin", Role="Administrator" },
+                new UserAccount{ Username = "user1", Password="pass1", Role = "User"}
+            };
+        }
+
+        public AuthenticationResponse? GenerateJwtToken(AuthenticationRequest authenticationRequest)
+        {
+            if (string.IsNullOrWhiteSpace(authenticationRequest.UserName) || string.IsNullOrWhiteSpace(authenticationRequest.Password)) { return null; }
+
+            var userAccount = _applicationUsers.Where(u => u.Username == authenticationRequest.UserName && u.Password == authenticationRequest.Password).FirstOrDefault();
+            if (userAccount == null) { return null; }
+
+            var tokenExpiryTimeStamp = DateTime.Now.AddMinutes(JWT_TOKEN_VALIDITY_MINS);
+            var tokenKey = Encoding.ASCII.GetBytes(JWT_SECURITY_KEY);
+            var claimsIdentity = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Name, authenticationRequest.UserName),
+                new Claim(ClaimTypes.Role, userAccount.Role)
+            });
+
+            var signingCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(tokenKey),
+                SecurityAlgorithms.HmacSha256Signature);
+
+            var securityTokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = claimsIdentity,
+                Expires = tokenExpiryTimeStamp,
+                SigningCredentials = signingCredentials
+            };
+
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var securityToken = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+            var token = jwtSecurityTokenHandler.WriteToken(securityToken);
+
+            return new AuthenticationResponse
+            {
+                UserName = userAccount.Username,
+                ExpiersIn = (int)tokenExpiryTimeStamp.Subtract(DateTime.Now).TotalSeconds,
+                JwtToken = token
+            };
+        }
+    }
+}
