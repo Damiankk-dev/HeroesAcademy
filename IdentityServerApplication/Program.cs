f4bf85efcc034bfa9773ad019f9bee48ff8a5f4c
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2023-05-06 20:46:55 +0200

    Basic checked IdentityServer

---
+++ IdentityServerApplication/Program.cs
@@ -0,0 +1,38 @@
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using IdentityServerApplication;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(Config.Clients)
+    .AddInMemoryIdentityResources(Config.IdentityResources)
+    .AddInMemoryApiResources(Config.ApiResources)
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddTestUsers(Config.TestUsers)
+    .AddDeveloperSigningCredential();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseIdentityServer();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
