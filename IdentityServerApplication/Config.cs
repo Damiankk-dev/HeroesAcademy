635da2883349d078fbca9cf903b3bdc106d6485f
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2023-05-06 21:11:22 +0200

    Set protected API config in IdentityServer

--- IdentityServerApplication/Config.cs
+++ IdentityServerApplication/Config.cs
@@ -1,5 +1,7 @@
 ﻿using IdentityServer4.Models;
 using IdentityServer4.Test;
+using Microsoft.AspNetCore.DataProtection;
+using System.Security.Cryptography;
 
 namespace IdentityServerApplication
 {
@@ -8,10 +10,21 @@ namespace IdentityServerApplication
         public static IEnumerable<Client> Clients =>
         new Client[]
         {
+            new Client
+            {
+                ClientId = "heroesAcademyClient",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets =
+                {
+                    new IdentityServer4.Models.Secret("secret".Sha256())
+                },
+                AllowedScopes = {"reservationsAPI"}
+            }
         };
         public static IEnumerable<ApiScope> ApiScopes =>
          new ApiScope[]
          {
+             new ApiScope("reservationsAPI", "Reservations API")
          };
         public static IEnumerable<ApiResource> ApiResources =>
          new ApiResource[]
@@ -26,4 +39,5 @@ namespace IdentityServerApplication
          {
          };
     }
+
 }
